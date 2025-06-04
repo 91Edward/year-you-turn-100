f78eed13bd9c2905facf93e5fee72185a961cb6a
91Edward <158228296+91Edward@users.noreply.github.com>
2025-06-04 22:13:39 +0100

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,11 @@
+name = input("Whate is your name?")
+
+age = int(input("What is your age?"))
+
+current_year = 2025;
+
+year_when_100 = current_year + (100 - age)
+
+print(f"Hy{name} You will turn 100 in the year {year_when_100}.")
+
+
